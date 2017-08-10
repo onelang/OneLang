1c6ced21a18bb7fc7efba4bda9d7deeb65e85f7c
KT <koczkatamas@gmail.com>
2017-08-11 01:37:33 +0200

    Sample program tester can work in single and multithreaded mode

--- SamplePrograms/HelloWorld/run.py
+++ SamplePrograms/HelloWorld/run.py
@@ -9,31 +9,40 @@ cmds = {
   "python": "python HelloWorld.py",
   "php": "php HelloWorld.php",
   "ruby": "ruby HelloWorld.rb",
-  "cpp": "g++ HelloWorld.cpp -o HelloWorld && ./HelloWorld && rm HelloWorld",
+  #"cpp": "g++ HelloWorld.cpp -o HelloWorld && ./HelloWorld && rm HelloWorld",
   "go": "go run HelloWorld.go",
   "csharp": "mcs HelloWorld.cs && mono HelloWorld.exe && rm HelloWorld.exe",
   "perl": "perl HelloWorld.pl",
-  "swift": "swift HelloWorld.swift",
-  "typescript": "tsc HelloWorld.ts --outFile HelloWorld.ts.js && node HelloWorld.ts.js && rm HelloWorld.ts.js"
+  #"swift": "swift HelloWorld.swift",
+  #"typescript": "tsc HelloWorld.ts --outFile HelloWorld.ts.js && node HelloWorld.ts.js && rm HelloWorld.ts.js",
 }
 
 results = []
-def thread_main(lang, cmd):
+def execLang(lang, cmd):
     start = time.time()
     response = subprocess.check_output(cmd, shell=True).replace("\n", "\\n")
     elapsedMs = (time.time() - start) * 1000
     results.append((lang, response, elapsedMs),)
 
 gstart = time.time()
-threads = []
-for item in cmds.iteritems():
-    t = threading.Thread(target=thread_main, args=item)
-    threads.append(t)
-    t.start()
-    
-for t in threads:
-    t.join()
-    
+
+def runMultithreaded():
+    threads = []
+    for item in cmds.iteritems():
+        t = threading.Thread(target=execLang, args=item)
+        threads.append(t)
+        t.start()
+        
+    for t in threads:
+        t.join()
+
+def runSequentially():
+    for item in cmds.iteritems():
+        execLang(*item)
+
+#runSequentially()
+runMultithreaded()
+
 gelapsedMs = (time.time() - gstart) * 1000
 
 print "elapsed: %dms" % gelapsedMs
