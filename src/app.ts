ed7d5e1629b2d60b04e48d4e185de03b0241c4bf
KT <koczkatamas@gmail.com>
2017-08-24 01:16:09 +0200

    Add peliminary expression & internal method handling

--- src/app.ts
+++ src/app.ts
@@ -54,6 +54,7 @@ namespace KsLangSchema {
         };
         array: string;
         templates: Templates;
+        expressions: string;
     }
 }
 
@@ -61,7 +62,7 @@ const prgName = "Test";
 const sourceCode = fs.readFileSync(`input/${prgName}.ts`, "utf8");
 const schema = TypeScriptParser.parseFile(sourceCode);
 const schemaJson = JSON.stringify(schema, null, 4);
-console.log(schemaJson);
+//console.log(schemaJson);
 fs.writeFileSync("schema.json", schemaJson);
 
 namespace CodeGeneratorSchema {
@@ -116,6 +117,34 @@ class CodeGenerator {
             return this.lang.primitiveTypes ? this.lang.primitiveTypes[type.type] : type.type;
     }
 
+    convertIdentifier(origName: string, vars: string[], mode: "variable"|"field"|"declaration") {
+        const name = origName === "class" ? "cls" : origName;
+        const isLocalVar = vars.includes(name);
+        return `${isLocalVar || mode === "declaration" || mode === "field" ? "" : "this."}${name}`;
+    }
+
+    getMethodPath(method: ks.Expression) {
+        let parts = [];
+        let currExpr = method;
+        while (true) {
+            if (currExpr.type === ks.ExpressionType.PropertyAccess) {
+                const propAcc = <ks.PropertyAccessExpression> currExpr;
+                if (propAcc.propertyName.type !== ks.ExpressionType.Identifier)
+                    return null;
+
+                parts.push((<ks.Identifier> propAcc.propertyName).text);
+                currExpr = propAcc.object;
+            } else if (currExpr.type === ks.ExpressionType.Identifier) {
+                parts.push((<ks.Identifier> currExpr).text);
+                break;
+            } else
+                return null;
+        }
+
+        const funcName = parts.reverse().map(x => x.toLowerCase()).join(".");
+        return funcName;
+    }
+
     generate() {
         const schema = <ks.SchemaFile>JSON.parse(JSON.stringify(this.schema));
         for (const enumName of Object.keys(schema.enums))
@@ -131,7 +160,8 @@ class CodeGenerator {
             }
         }
 
-        const vm = <CodeGeneratorSchema.Root> {
+        const self = this;
+        const vm = { // <CodeGeneratorSchema.Root> 
             absoluteIncludes: [],
             classes: Object.keys(schema.classes).map(className => {
                 const cls = schema.classes[className];
@@ -158,7 +188,53 @@ class CodeGenerator {
                     privateMethods: []
                 };
             }),
+            expressionGenerators: <{ [name: string]: (expr: any) => string }> {},
+            internalMethodGenerators: <{ [name: string]: (expr: any) => string }> {},
+            gen: function (obj: ks.Statement|ks.Expression) {
+                if (obj.type === ks.StatementType.Expression)
+                    obj = (<ks.ExpressionStatement>obj).expression;
+
+                if (obj.type === ks.ExpressionType.Call) {
+                    const callExpr = <ks.CallExpression> obj;
+                    const methodPath = self.getMethodPath(callExpr.method);
+                    const method = methodPath && self.lang.functions[methodPath];
+                    if (method) {
+                        if (method.arguments.length !== callExpr.arguments.length)
+                            throw new Error(`Invalid argument count for '${methodPath}': expected: ${method.arguments.length}, actual: ${callExpr.arguments.length}.`);
+
+                        const args = callExpr.arguments.map(x => this.gen(x));
+                        const code = this.internalMethodGenerators[methodPath].apply(this, args);
+                        return code;
+                    }
+                }
+
+                const genFunc = this.expressionGenerators[obj.type];
+                if (!genFunc)
+                    throw new Error(`Expression template not found: ${obj.type}!`);
+                const result = genFunc.call(this, obj);
+
+                console.log("generate statement", obj, result);
+
+                return result;
+            },
+            main: <() => string> null,
         };
+
+        for (const exprName of Object.keys(this.lang.expressions)) {
+            const exprTmpl = this.lang.expressions[exprName];
+            const tmpl = new Template(exprTmpl, ["expr"]);
+            tmpl.convertIdentifier = this.convertIdentifier;
+            const genFunc = tmpl.getGeneratorFunction();
+            vm.expressionGenerators[exprName.ucFirst()] = genFunc;
+        }
+
+        for (const funcPath of Object.keys(this.lang.functions)) {
+            const funcInfo = this.lang.functions[funcPath];
+            const tmpl = new Template(funcInfo.template, funcInfo.arguments.map(x => x.name));
+            tmpl.convertIdentifier = this.convertIdentifier;
+            const genFunc = tmpl.getGeneratorFunction();
+            vm.internalMethodGenerators[funcPath] = genFunc;
+        }
 
         for (const tmplName of Object.keys(this.lang.templates)) {
             const tmplOrig = this.lang.templates[tmplName];
@@ -166,11 +242,7 @@ class CodeGenerator {
             if (tmplName === "testGenerator")
                 tmplObj.args = [{ name: "cls" }, { name: "method" }];
             const tmpl = new Template(tmplObj.template, tmplObj.args.map(x => x.name));
-            tmpl.convertIdentifier = (origName, vars, mode) => {
-                const name = origName === "class" ? "cls" : origName;
-                const isLocalVar = vars.includes(name);
-                return `${isLocalVar || mode === "declaration" || mode === "field" ? "" : "this."}${name}`;
-            };
+            tmpl.convertIdentifier = this.convertIdentifier;
             const genFunc = tmpl.getGeneratorFunction();
             vm[tmplName] = genFunc;
         }
@@ -188,10 +260,14 @@ class Utils {
     }
 }
 
-for (const langFn of fs.readdirSync("langs")) {
+let langs = fs.readdirSync("langs");
+langs = ["csharp.yaml"];
+for (const langFn of langs) {
     const langName = langFn.replace(".yaml", "");
     const langSchema = <KsLangSchema.LangFile> YAML.parse(fs.readFileSync(`langs/${langFn}`, "utf8"));
     const codeGenerator = new CodeGenerator(schema, langSchema);
     const generatedCode = codeGenerator.generate();
     Utils.writeFile(`SamplePrograms/${prgName}/${prgName}.${langSchema.extension}`, generatedCode);
+    console.log(generatedCode);
 }
+debugger;
