1288ab608a39fc6a70f052b3ef86f2a59bd257b0
KT <koczkatamas@gmail.com>
2017-08-27 16:19:23 +0200

    Move Java one level up, improve security, only measure compile time
    Security: bind only to localhost, prevent DNS rebinding attack

--- FastCompile/Java/src/main/java/fastjavacompile/App.java
+++ FastCompile/Java/src/main/java/fastjavacompile/App.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
 import com.google.gson.Gson;
@@ -19,6 +20,13 @@ import com.sun.net.httpserver.HttpServer;
  */
 public class App
 {
+    public static String exceptionToString(Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        String result = sw.toString();
+        return result;
+    }
+
     static class CompileHandler implements HttpHandler {
         private class Request {
             public String code;
@@ -44,68 +52,73 @@ public class App
             }
         }
 
-        public void sendResponse(HttpExchange t, Response response) throws IOException {
-            String responseJson = new Gson().toJson(response);
+        public void sendTextResponse(HttpExchange t, int statusCode, String data) throws IOException {
             t.getResponseHeaders().add("Access-Control-Allow-Origin", "http://localhost:8000");
-            t.sendResponseHeaders(200, responseJson.length());
+            byte[] responseBytes = data.getBytes();
+            t.sendResponseHeaders(200, responseBytes.length);
             OutputStream os = t.getResponseBody();
-            os.write(responseJson.getBytes());
+            os.write(responseBytes);
             os.close();
         }
 
-        public String exceptionToString(Exception e) {
-            StringWriter sw = new StringWriter();
-            e.printStackTrace(new PrintWriter(sw));
-            String result = sw.toString();
-            return result;
-    }
+        public void sendResponse(HttpExchange t, Response response) throws IOException {
+            String responseJson = new Gson().toJson(response);
+            sendTextResponse(t, 200, responseJson);
+        }
 
         @Override
         public void handle(HttpExchange t) throws IOException {
-            System.out.println("Before parsing request");
-            Request request = getRequest(t);
-            System.out.println("After parsing request");
-
             Response response = new Response();
-            long startTime = System.nanoTime();
             
             try {
-                System.out.println("Before compile");
+                // protection against DNS rebinding attack
+                String host = t.getRequestHeaders().get("Host").get(0);
+                if (!host.startsWith("127.0.0.1")) {
+                    sendTextResponse(t, 403, "Host not allowed: " + host);
+                    return;
+                }
+
+                Request request = getRequest(t);
+
+                long startTime = System.nanoTime();
                 Class<?> helloClass = InMemoryJavaCompiler.compile(request.className, request.code);
-                System.out.println("After compile");
+                response.elapsedMs = (System.nanoTime() - startTime) / 1000 / 1000;
+
                 Constructor<?> ctor = helloClass.getConstructor();
                 Object instance = ctor.newInstance();
                 Method testMethod = helloClass.getMethod(request.methodName);
-                System.out.println("Before invoke");
                 response.result = testMethod.invoke(instance);
-                System.out.println("After invoke");
             } catch(Exception e) {
                 response.exceptionText = exceptionToString(e);
+                Log("Exception (handle): " + response.exceptionText);
             }
-
-            response.elapsedMs = (System.nanoTime() - startTime) / 1000 / 1000;
-            System.out.println("Compile: " + response.elapsedMs + "ms");
 
             sendResponse(t, response);
         }
     }
 
+    public static void Log(String text) {
+        System.out.println("[Java] " + text);
+    }
+
     public static void main(String[] args)
     {
         try {
-            System.out.println("Started.");
+            Log("Started.");
 
             int port = args.length > 0 ? Integer.parseInt(args[0]) : 8001;
             
-            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(), port), 0);
             server.createContext("/compile", new CompileHandler());
             server.start();
-    
-            System.out.println("Server started.");
+            Log("Server listening on port " + port);
+
+            System.in.read();
+            server.stop(0);
         } catch(Exception e) {
-            System.out.println(e);
+            Log("Exception (main): " + exceptionToString(e));
         }
 
-        System.out.println("Main done.");
+        Log("Server stopped.");
     }
 }
