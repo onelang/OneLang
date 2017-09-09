9044a124913a7c0be59e485ead816e538429df45
KT <koczkatamas@gmail.com>
2017-09-09 23:55:43 +0200

    Treat PHP warnings as errors

--- FastCompile/PHP/server.php
+++ FastCompile/PHP/server.php
@@ -1,4 +1,9 @@
 <?php
+function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
+
 header("Access-Control-Allow-Origin: http://127.0.0.1:8000");
 try {
     $postdata = json_decode(file_get_contents("php://input"), true);
@@ -13,5 +18,5 @@ try {
     $elapsedMs = (int)((microtime(true) - $startTime) * 1000);
     print json_encode(array("result" => $result, "elapsedMs" => $elapsedMs));
 } catch(Exception $e) {
-    print json_encode(array("exceptionText" => $e->getTraceAsString()));
+    print json_encode(array("exceptionText" => "line #{$e->getLine()}: {$e->getMessage()}\n{$e->getTraceAsString()}"));
 }
