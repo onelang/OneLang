52d53256f80efd32ed4e822abe0a1823d875279b
KT <koczkatamas@gmail.com>
2017-10-01 01:07:10 +0200

    Cpp: compiler uses C++11

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ langs = {
     },
     "CPP": {
         "ext": "cpp",
-        "cmd": "g++ {name}.cpp -o {name} && ./{name} && rm {name}"
+        "cmd": "g++ -std=c++11 {name}.cpp -o {name} && ./{name} && rm {name}"
     },
     "Go": {
         "ext": "go",
