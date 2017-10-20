9b99929cfae0ede50b340f5d754011673d447471
KT <koczkatamas@gmail.com>
2017-10-21 00:24:51 +0200

    FastCompile: Ruby: redir stdout

--- FastCompile/Ruby/server.rb
+++ FastCompile/Ruby/server.rb
@@ -1,21 +1,28 @@
 require 'webrick'
 require 'json'
+require 'stringio'
 
 nullLog = WEBrick::Log.new(File.open(File::NULL, 'w'))
 server = WEBrick::HTTPServer.new :Port => 8005, :AccessLog => [], :BindAddress => "127.0.0.1", :Logger => nullLog
 trap 'INT' do server.shutdown end
 
 server.mount_proc '/compile' do |req, res|
-    res["Access-Control-Allow-Origin"] = "*"
+    original_stdout = $stdout
     begin
+        res["Access-Control-Allow-Origin"] = "*"
+
+        $stdout = result_stdout = StringIO.new
         request = JSON.parse(req.body())
-        code = "#{request['code']}\n#{request['className']}.new().#{request['methodName']}"
+
         startTime = Time.now
+        result = eval request['code']
         elapsedMs = ((Time.now - startTime) * 1000).round
-        result = eval code
-        res.body = JSON.generate({ :result => result, :elapsedMs => elapsedMs })
+        
+        res.body = JSON.generate({ :result => $stdout.string, :elapsedMs => elapsedMs })
     rescue
         res.body = JSON.generate({ :exceptionText => "#{$@.first}: #{$!.message} (#{$!.class})" + $@.drop(1).map{|s| "\n\t#{s}"}.join("") })
+    ensure
+        $stdout = original_stdout
     end
 end
 
