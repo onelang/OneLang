f9ad140267c1064efd7e545f2caafb94dfd52eec
KT <koczkatamas@gmail.com>
2017-12-26 17:53:55 +0100

    Add more checks to ReflectionTest

--- input/ReflectionTest.ts
+++ input/ReflectionTest.ts
@@ -15,31 +15,39 @@ class TargetClass {
 
 class TestClass {
     testMethod() {
-        var obj = new TargetClass();
+        const obj = new TargetClass();
         //console.log(`instanceMethod (direct): ${obj.instanceMethod()}`);
         //console.log(`staticMethod (direct): ${TargetClass.staticMethod("arg1value")}`);
         //console.log(`instanceField (direct): ${obj.instanceField}`);
         //console.log(`staticField (direct): ${TargetClass.staticField}`);
-        var cls = OneReflect.getClass(obj);
+        const cls = OneReflect.getClass(obj);
         if (cls === null)
             console.log("cls is null!");
 
-        var cls2 = OneReflect.getClassByName("TargetClass");
+        const cls2 = OneReflect.getClassByName("TargetClass");
         if (cls2 === null)
             console.log("cls2 is null!");
 
-        var method1 = cls.getMethod("instanceMethod");
+        const method1 = cls.getMethod("instanceMethod");
         if (method1 === null)
             console.log("method1 is null!");
 
-        var method1Result = method1.call(obj, []);
+        const method1Result = method1.call(obj, []);
         console.log(`instanceMethod: ${method1Result}`);
 
-        var method2 = cls.getMethod("staticMethod");
+        const method2 = cls.getMethod("staticMethod");
         if (method2 === null)
             console.log("method2 is null!");
 
-        var method2Result = method2.call(null, ["arg1value"]);
+        const method2Result = method2.call(null, ["arg1value"]);
         console.log(`staticMethod: ${method2Result}`);
+
+        const field1 = cls.getField("instanceField");
+        field1.setValue(obj, 6);
+        console.log(`new instance field value: ${obj.instanceField}`);
+
+        const field2 = cls.getField("staticField");
+        field2.setValue(null, "bello");
+        console.log(`new static field value: ${TargetClass.staticField}`);
     }
 }
