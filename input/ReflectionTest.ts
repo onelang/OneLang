7466a6248dda8a410e917ea1f08c6f70777567cd
KT <koczkatamas@gmail.com>
2017-12-27 19:13:08 +0100

    More null checks in ReflectionTest

--- input/ReflectionTest.ts
+++ input/ReflectionTest.ts
@@ -21,41 +21,28 @@ class TestClass {
         //console.log(`instanceField (direct): ${obj.instanceField}`);
         //console.log(`staticField (direct): ${TargetClass.staticField}`);
         const cls = OneReflect.getClass(obj);
-        if (cls === null) {
-            console.log("cls is null!");
-            return;
-        }
-
+        if (cls === null) { console.log("cls is null!"); return; }
         const cls2 = OneReflect.getClassByName("TargetClass");
-        if (cls2 === null) {
-            console.log("cls2 is null!");
-            return;
-        }
+        if (cls2 === null) { console.log("cls2 is null!"); return; }
 
         const method1 = cls.getMethod("instanceMethod");
-        if (method1 === null) {
-            console.log("method1 is null!");
-            return;
-        }
-
+        if (method1 === null) { console.log("method1 is null!"); return; }
         const method1Result = method1.call(obj, []);
         console.log(`instanceMethod: ${method1Result}`);
 
         const method2 = cls.getMethod("staticMethod");
-        if (method2 === null) {
-            console.log("method2 is null!");
-            return;
-        }
-
+        if (method2 === null) { console.log("method2 is null!"); return; }
         const method2Result = method2.call(null, <any[]>["arg1value"]);
         console.log(`staticMethod: ${method2Result}`);
 
         const field1 = cls.getField("instanceField");
+        if (field1 === null) { console.log("field1 is null!"); return; }
         field1.setValue(obj, 6);
         const field1NewVal = field1.getValue(obj);
         console.log(`new instance field value: ${obj.instanceField} == ${field1NewVal}`);
 
         const field2 = cls.getField("staticField");
+        if (field2 === null) { console.log("field2 is null!"); return; }
         field2.setValue(null, "bello");
         const field2NewVal = field2.getValue(null);
         console.log(`new static field value: ${TargetClass.staticField} == ${field2NewVal}`);
