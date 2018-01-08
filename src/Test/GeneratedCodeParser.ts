763392fd31ee64cefeb8b3fec6e77e4be7483615
KT <koczkatamas@gmail.com>
2018-01-08 23:30:38 +0100

    [test/GenCodeParser] add Ruby support, refactor

--- src/Test/GeneratedCodeParser.ts
+++ src/Test/GeneratedCodeParser.ts
@@ -4,35 +4,41 @@ import { readFile, writeFile } from "../Utils/NodeUtils";
 import { OneAst as ast } from "../One/Ast";
 import { AstHelper } from "../One/AstHelper";
 import { OneCompiler } from "../OneCompiler";
+import { RubyParser } from "../Parsers/RubyParser";
 require("../Utils/Extensions.js");
 const fs = require("fs");
 global["YAML"] = require('yamljs'); 
 declare var YAML;
 
-const prgNames = fs.readdirSync("generated");
-const prgExcludeList = ["stdlib", "overlay", "TemplateTests"];
+let prgNames = fs.readdirSync("generated");
+let prgExcludeList = ["stdlib", "overlay", "TemplateTests"];
 
 const stdlibCode = readFile(`langs/StdLibs/stdlib.d.ts`);
 const genericTransforms = readFile(`langs/NativeResolvers/GenericTransforms.yaml`);
 
-for (const ext of ["ts", "cs"]) {
-    const langName = ext === "ts" ? "typescript" : "csharp";
+const langs: { [langName: string]: { ext: string, parse: (src: string) => ast.Schema } } = {    
+    typescript: { ext: "ts", parse: src => TypeScriptParser2.parseFile(src) },
+    csharp: { ext: "cs", parse: src => CSharpParser.parseFile(src) },
+    ruby: { ext: "rb", parse: src => RubyParser.parseFile(src) },
+};
+
+let langsToTest = Object.keys(langs);
+
+langsToTest = ["ruby"];
+//prgExcludeList = [...prgExcludeList, "OneLang2", "StrReplaceTest"]
+
+for (const langName of langsToTest) {
+    const langData = langs[langName];
     const overlayCode = readFile(`langs/NativeResolvers/${langName}.ts`);
 
     for (const prgName of prgNames) {
         if (prgExcludeList.includes(prgName)) continue;
-        const fn = `generated/${prgName}/results/${prgName}.${ext}`;
+        const fn = `generated/${prgName}/results/${prgName}.${langData.ext}`;
         console.log(`Parsing '${fn}'...`);
         let content = readFile(fn);
-
-        let schema: ast.Schema;
-        if (ext === "ts") {
-            schema = TypeScriptParser2.parseFile(content);
-        } else if (ext === "cs") {
-            schema = CSharpParser.parseFile(content);
-        }
-
-        writeFile(`generated/${prgName}/regen/0_Original_${ext}.json`, AstHelper.toJson(schema));
+        
+        const schema = langData.parse(content);
+        writeFile(`generated/${prgName}/regen/0_Original_${langData.ext}.json`, AstHelper.toJson(schema));
 
         const compiler = new OneCompiler();
         compiler.parse(langName, content, overlayCode, stdlibCode, genericTransforms);
