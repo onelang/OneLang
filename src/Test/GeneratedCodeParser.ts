0ba00f7da4d420382edd7dad5235f4bfe555a481
KT <koczkatamas@gmail.com>
2018-01-10 22:17:08 +0100

    [test/GenCodeParser] save regen schema states

--- src/Test/GeneratedCodeParser.ts
+++ src/Test/GeneratedCodeParser.ts
@@ -42,6 +42,10 @@ for (const langName of langsToTest) {
         writeFile(`generated/${prgName}/regen/0_Original_${langData.ext}.json`, AstHelper.toJson(schema));
 
         const compiler = new OneCompiler();
+        compiler.saveSchemaStateCallback = (type: "overviewText"|"schemaJson", schemaType: "program"|"overlay"|"stdlib", name: string, data: string) => {
+            if (schemaType !== "program") return;
+            writeFile(`generated/${prgName}/regen/schemaStates_${langName}/${name}.${type === "overviewText" ? "txt" : "json"}`, data);
+        };
         compiler.parse(langName, content, overlayCode, stdlibCode, genericTransforms);
     }
 }
