f5d879d606c2f56e873c2dd063898abf9f7c3bf7
KT <koczkatamas@gmail.com>
2018-01-17 01:15:13 +0100

    Add index.html to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 cd ../ide.onelang.io
 rm -rf js input langs lib src
 cd ../onelang
-cp -R js input langs lib src ../ide.onelang.io/
+cp -R js input langs lib src index.html ../ide.onelang.io/
 cd ../ide.onelang.io/
 git commit -am "Update (koczkatamas/onelang@`cd ../onelang;git rev-parse HEAD`)"
