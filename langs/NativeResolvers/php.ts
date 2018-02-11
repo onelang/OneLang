885d7f4903f34a1549396c8a0be2a92b20d9964b
KT <koczkatamas@gmail.com>
2018-02-11 16:52:27 +0100

    [PHP] Add some basic types to native resolvers

--- langs/NativeResolvers/php.ts
+++ langs/NativeResolvers/php.ts
@@ -1,3 +1,19 @@
 class PhpArray<T> {
     _one: OneArray<T>;
 }
+
+class PhpMap<K,V> {
+    _one: OneMap<K,V>;
+}
+
+class PhpString {
+    _one: OneString;
+}
+
+class PhpBoolean {
+    _one: OneBoolean;
+}
+
+class PhpNumber {
+    _one: OneNumber;
+}
