b80c20dc18ca362a4e5f9eb80c63690386de4775
KT <koczkatamas@gmail.com>
2018-02-12 20:53:11 +0100

    [input/MultiLangTest] Add new constructs

--- input/MultiLangTest.ts
+++ input/MultiLangTest.ts
@@ -4,4 +4,23 @@ class Calculator {
     }
 }
 
-console.log("Hello world!");
+console.log("Hello!");
+
+const calc = new Calculator();
+console.log(`n = ${calc.calc()}`);
+
+const arr = [1, 2, 3];
+const map = { a: 2, b: 3 };
+console.log(`map['a'] = ${map['a']}, arr[1] = ${arr[1]}`);
+
+if (arr[0] == 1) {
+    console.log("TRUE-X");
+} else {
+    console.log("FALSE");
+}
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum += i + 2;
+}
+console.log(sum);
