a3a928d566ada586b0fa1f55cbda386305f51cd0
KT <koczkatamas@gmail.com>
2018-02-18 00:00:01 +0100

    [Publish] Add Google Analytics to published code

--- publish.sh
+++ publish.sh
@@ -3,4 +3,5 @@ rm -rf js input langs lib src
 cd ../onelang
 cp -R js input langs lib src index.html ../ide.onelang.io/
 cd ../ide.onelang.io/
+python ../onelang/publish.py
 git add -A && git commit -m "Update (koczkatamas/onelang@`cd ../onelang;git rev-parse HEAD`)"
