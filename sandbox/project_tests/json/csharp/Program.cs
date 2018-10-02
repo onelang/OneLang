e95b0cd82ec00bc56532faba225ea62af7bcb6b2
KT <koczkatamas@gmail.com>
2018-10-02 17:26:00 +0200

    [Sandbox] Add JSON C# project test

---
+++ sandbox/project_tests/json/csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = JObject.Parse("{'name':'value'}");
+            Console.WriteLine($"value = {obj["name"]}");
+        }
+    }
+}
