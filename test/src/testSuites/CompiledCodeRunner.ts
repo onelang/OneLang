a0bc0c6ceda60c4a37a05a135f03c24f794f164a
KT <koczkatamas@gmail.com>
2020-02-15 22:41:20 +0100

    [Test] CompiledCodeRunner tests against expected results

--- test/src/testSuites/CompiledCodeRunner.ts
+++ test/src/testSuites/CompiledCodeRunner.ts
@@ -1,27 +1,47 @@
 import 'module-alias/register';
-import { getCompilationTestPrgNames, getLangFiles, readFile, jsonRequest, assert } from '../TestUtils';
+import { getCompilationTestPrgNames, getLangFiles, readFile, jsonRequest, assert, getYamlTestSuite, timeNow } from '../TestUtils';
 
-export interface CompileResult {
-    result?: string;
-    elapsedMs?: number;
-    exceptionText?: string;
-}
+//const filter: RegExp = /cpp:(ArrayTest|HelloWorld|HelloWorldRaw)/;
+const filter: RegExp = /:FileTest/;
 
-//const prgs = getCompilationTestPrgNames();
-const prgs = ["HelloWorldRaw"];
+const backendUrl = "http://127.0.0.1:11111";
+const expectedResults: { [prgName: string]: string } = getYamlTestSuite("CompiledCodeRunner");
+const prgs = getCompilationTestPrgNames();
 const langs = getLangFiles();
+
+// init compiler backend
+jsonRequest(`${backendUrl}/api/status`, null);
+
 for (const prg of prgs) {
     describe(prg, () => {
         for (const langName of Object.keys(langs)) {
+            if (filter && !filter.exec(`${langName}:${prg}`)) continue;
             const lang = langs[langName];
             const [mainClass, mainMethod] = (lang.main || "").split('.');
             it(langName, async () => {
                 const sourceCode = readFile(`test/artifacts/CompilationTest/${prg}/results/${prg}.${lang.extension}`);
                 const request = { lang: langName, code: sourceCode, packageSources: [], className: mainClass, methodName: mainMethod };
-                const result = await jsonRequest<CompileResult>(`http://127.0.0.1:11111/compile?useCache`, request);
-                if (result.exceptionText)
-                    throw new Error(result.exceptionText);
+                let res = await jsonRequest<CompileResult>(`${backendUrl}/compile?useCache`, request);
+                
+                if (res.exceptionText && res.fromCache)
+                    res = await jsonRequest<CompileResult>(`${backendUrl}/compile`, request);
+
+                if (res.exceptionText)
+                    throw new Error(res.exceptionText);
+
+                const expected = expectedResults[prg];
+                if (typeof expected !== "undefined")
+                    assert.equal(res.result, expected);
+                else if (langName === "javascript")
+                    console.error(`Expected result was not set for program "${prg}", current result is ${JSON.stringify(res.result)}`);
             })
         }
     })
+}
+
+interface CompileResult {
+    result?: string;
+    elapsedMs?: number;
+    exceptionText?: string;
+    fromCache: boolean;
 }
