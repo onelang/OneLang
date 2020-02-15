40f665a63f79bf4bff967e6409066fd604516db2
KT <koczkatamas@gmail.com>
2020-02-15 22:48:31 +0100

    [Test] Add missing change from FileTest

--- test/testSuites/CompilationTest/FileTest.ts
+++ test/testSuites/CompilationTest/FileTest.ts
@@ -1,4 +1,3 @@
-const fileContent = OneFile.readText("../../../input/test.txt");
 OneFile.writeText("test.txt", "example content");
 const fileContent = OneFile.readText("test.txt");
 console.log(fileContent);
