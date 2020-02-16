95daaaf8191ee2a933ee9615fe8000e181085fb0
KT <koczkatamas@gmail.com>
2020-02-16 20:03:55 +0100

    [Test] Fix ReflectionTest

--- packages/implementations/OneLang-Reflect-v0.1/native/one.hpp
+++ packages/implementations/OneLang-Reflect-v0.1/native/one.hpp
@@ -15,15 +15,15 @@ class ReflectedClass
 
 class OneReflectUtil {
   public:
-    static string nameKey(const string& name) {
-        string key = OneStringHelper::replace(name, "_", "");
+    static std::string nameKey(const std::string& name) {
+        std::string key = OneStringHelper::replace(name, "_", "");
         std::transform(key.begin(), key.end(), key.begin(), ::tolower);
         return key;
     }
 };
 
-typedef any (*OneFieldGetter)(one::sp<ReflectedClass> obj);
-typedef void (*OneFieldSetter)(pne::sp<ReflectedClass> obj, any value);
+typedef std::any (*OneFieldGetter)(one::sp<ReflectedClass> obj);
+typedef void (*OneFieldSetter)(one::sp<ReflectedClass> obj, std::any value);
 
 class OneField {
   public:
@@ -35,14 +35,14 @@ class OneField {
     OneFieldGetter getter;
     OneFieldSetter setter;
 
-    OneField(const string& name, bool isStatic, const string& type, OneFieldGetter getter, OneFieldSetter setter)
+    OneField(const std::string& name, bool isStatic, const std::string& type, OneFieldGetter getter, OneFieldSetter setter)
         : name(name), isStatic(isStatic), type(type), getter(getter), setter(setter) { }
 
-    any getValue(one::sp<ReflectedClass> obj) {
+    std::any getValue(one::sp<ReflectedClass> obj) {
         return this->getter(obj);
     }
 
-    void setValue(one::sp<ReflectedClass> obj, any value) {
+    void setValue(one::sp<ReflectedClass> obj, std::any value) {
         this->setter(obj, value);
     }
 };
@@ -56,12 +56,12 @@ class OneMethodArgument {
         : name(name), type(type) { }
 };
 
-typedef any (*OneMethodInvoker)(one::sp<ReflectedClass> obj, one::vec<any> args);
+typedef std::any (*OneMethodInvoker)(one::sp<ReflectedClass> obj, one::vec<std::any> args);
 
 class OneMethod {
   public:
     one::sp<OneClass> cls;
-    string name;
+    std::string name;
     bool isStatic;
     std::string returnType;
     std::vector<OneMethodArgument> args;
@@ -70,7 +70,7 @@ class OneMethod {
     OneMethod(const std::string& name, bool isStatic, const std::string& returnType, const std::vector<OneMethodArgument>&& args, OneMethodInvoker invoker)
         : name(name), isStatic(isStatic), returnType(returnType), args(args), invoker(invoker) { }
 
-    any call(one::sp<ReflectedClass> obj, one::vec<any> args) {
+    std::any call(one::sp<ReflectedClass> obj, one::vec<std::any> args) {
         return this->invoker(obj, args);
     }
 };
@@ -78,7 +78,7 @@ class OneMethod {
 class OneClass {
     public:
     std::string name;
-    const type_info& typeInfo;
+    const std::type_info& typeInfo;
     std::map<std::string, one::sp<OneField>> fields;
     std::map<std::string, one::sp<OneMethod>> methods;
 
@@ -95,8 +95,8 @@ class OneClass {
         return *this;
     }
 
-    one::sp<OneField> getField(const string& name) { return this->fields[OneReflectUtil::nameKey(name)]; }
-    one::sp<OneMethod> getMethod(const string& name) { return this->methods[OneReflectUtil::nameKey(name)]; }
+    one::sp<OneField> getField(const std::string& name) { return this->fields[OneReflectUtil::nameKey(name)]; }
+    one::sp<OneMethod> getMethod(const std::string& name) { return this->methods[OneReflectUtil::nameKey(name)]; }
 
     one::vec<one::sp<OneField>> getFields() {
         one::vec<one::sp<OneField>> result;
@@ -115,11 +115,12 @@ class OneClass {
 
 class OneReflect {
   public:
-    static std::map<string, one::sp<OneClass>> classesByName;
+    static std::map<std::string, one::sp<OneClass>> classesByName;
     static std::map<size_t, one::sp<OneClass>> classesByTypeHash;
 
-    static one::sp<OneClass> getClass(one::sp<ReflectedClass> cls) {
-        return classesByTypeHash[typeid(*cls).hash_code()];
+    template <typename T>
+    static one::sp<OneClass> getClass(one::sp<T> cls) {
+        return classesByTypeHash[typeid(T).hash_code()];
     }
 
     static one::sp<OneClass> getClassByName(const std::string& name) {
@@ -137,15 +138,15 @@ class OneReflect {
 std::map<std::string, one::sp<OneClass>> OneReflect::classesByName;
 std::map<size_t, one::sp<OneClass>> OneReflect::classesByTypeHash;
 
-std::string operator+(const std::string& str, const any& value) {
+std::string operator+(const std::string& str, const std::any& value) {
     try {
         auto& type = value.type();
         if (type == typeid(int)) {
-            return str + std::to_string(any_cast<int>(value));
-        } else if (type == typeid(string)) {
+            return str + std::to_string(std::any_cast<int>(value));
+        } else if (type == typeid(std::string)) {
             return str + std::any_cast<std::string>(value);
         } else {
-            return str + "any<" + type.name() + ">";
+            return str + "std::any<" + type.name() + ">";
         }
     }
     catch(const std::exception& e) {
