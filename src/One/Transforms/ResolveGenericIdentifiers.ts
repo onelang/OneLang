90dd89366fea2b0237dedf7d1960b8109d59aefb
KT <koczkatamas@gmail.com>
2020-02-26 02:03:40 +0100

    [Trans] Add ResolveGenericIdentifiers transform

---
+++ src/One/Transforms/ResolveGenericIdentifiers.ts
@@ -0,0 +1,37 @@
+import { AstTransformer } from "../AstVisitor";
+import { Type, UnresolvedType, GenericsType } from "../Ast/AstTypes";
+import { Class, Method } from "../Ast/Types";
+
+/**
+ * After parsing an input source code file, it is not known that a "T" is 
+ *   an unknown type called "T" or it is a generic type, as it only turns
+ *   out when a class or method is flagged with <T> in its declaration.
+ * 
+ * So this tranform modifies "T" from (unknown) type "T" to generic type "T"
+ *   in classes "ExampleClass<T> { ... T ... }" 
+ *   and methods "ExampleMethod<T>(...) { ... T ... }"
+ */
+export class ResolveGenericIdentifiers extends AstTransformer<void> {
+    currentClass: Class;
+    currentMethod: Method;
+
+    protected visitClass(cls: Class) {
+        this.currentClass = cls;
+        super.visitClass(cls);
+    }
+
+    protected visitMethod(method: Method) {
+        this.currentMethod = method;
+        super.visitMethod(method);
+    }
+
+    protected visitType(type: Type) {
+        super.visitType(type);
+
+        //console.log(type && type.constructor.name, JSON.stringify(type));
+        if (type instanceof UnresolvedType && 
+            ((this.currentClass && this.currentClass.typeArguments.includes(type.typeName)) ||
+            (this.currentMethod && this.currentMethod.typeArguments.includes(type.typeName))))
+            return new GenericsType(type.typeName);
+    }
+}
