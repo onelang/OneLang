eeac95caff9fb61aa718a6b00f34374efd52ad6f
KT <koczkatamas@gmail.com>
2020-09-10 23:05:39 +0200

    [Test] JS SelfTest set return code to 1 on fail

--- test/src/SelfTest.ts
+++ test/src/SelfTest.ts
@@ -1,4 +1,5 @@
 import 'module-alias/register';
+import 'process';
 
 process.env.NODE_PATH = `${__dirname}/../../onepkg`;
 require("module").Module._initPaths();
@@ -6,4 +7,4 @@ require("module").Module._initPaths();
 import { SelfTestRunner } from "@one/Test/SelfTestRunner";
 import { CsharpGenerator } from '@one/Generator/CsharpGenerator';
 
-new SelfTestRunner(`${__dirname}/../../`).runTest(new CsharpGenerator());
+new SelfTestRunner(`${__dirname}/../../`).runTest(new CsharpGenerator()).then(res => process.exit(res ? 0 : 1));
