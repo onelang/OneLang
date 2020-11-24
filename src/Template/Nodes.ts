d34134fa50cba1671c07abb5f53244a7c3035cbe
KT <koczkatamas@gmail.com>
2020-11-24 21:45:57 +0100

    [Tmpl/Format] Add value formatting hook, use context instead of just model

--- src/Template/Nodes.ts
+++ src/Template/Nodes.ts
@@ -1,43 +1,57 @@
-import { Expression } from "../One/Ast/Expressions";
+import { ExpressionValue } from "../Generator/TemplateFileGeneratorPlugin";
+import { Expression, StringLiteral } from "../One/Ast/Expressions";
 import { TSOverviewGenerator } from "../Utils/TSOverviewGenerator";
 import { ExprVM } from "../VM/ExprVM";
-import { ArrayValue, ObjectValue, StringValue } from "../VM/Values";
+import { ArrayValue, IVMValue, ObjectValue, StringValue } from "../VM/Values";
 
 export interface ITemplateNode {
-    format(model: ObjectValue): string;
+    format(context: TemplateContext): string;
+}
+
+export interface ITemplateFormatHooks {
+    formatValue(value: IVMValue): string;
+}
+
+export class TemplateContext {
+    constructor(public model: ObjectValue, public hooks: ITemplateFormatHooks = null) { }
 }
 
 export class TemplateBlock implements ITemplateNode {
     constructor(public items: ITemplateNode[]) { }
 
-    format(model: ObjectValue): string { 
-        return this.items.map(x => x.format(model)).join("");
+    format(context: TemplateContext): string { 
+        return this.items.map(x => x.format(context)).join("");
     }
 }
 
 export class LiteralNode implements ITemplateNode {
     constructor(public value: string) { }
 
-    format(model: ObjectValue): string { return this.value; }
+    format(context: TemplateContext): string { return this.value; }
 }
 
 export class ExpressionNode implements ITemplateNode {
     constructor(public expr: Expression) { }
 
-    format(model: ObjectValue): string {
-        const result = new ExprVM(model).evaluate(this.expr);
-        if (result instanceof StringValue)
-            return result.value;
-        else
-            throw new Error(`ExpressionNode (${TSOverviewGenerator.preview.expr(this.expr)}) return a non-string result!`);
+    format(context: TemplateContext): string {
+        const value = new ExprVM(context.model).evaluate(this.expr);
+        if (value instanceof StringValue) return value.value;
+
+        if (context.hooks !== null) {
+            const result = context.hooks.formatValue(value);
+            if (result !== null)
+                return result;
+        }
+
+        throw new Error(`ExpressionNode (${TSOverviewGenerator.preview.expr(this.expr)}) return a non-string result!`);
     }
 }
 
 export class ForNode implements ITemplateNode {
     constructor(public variableName: string, public itemsExpr: Expression, public body: TemplateBlock, public joiner: string) { }
 
-    format(model: ObjectValue): string {
-        const items = new ExprVM(model).evaluate(this.itemsExpr);
+    format(context: TemplateContext): string {
+        const items = new ExprVM(context.model).evaluate(this.itemsExpr);
         if (!(items instanceof ArrayValue))
             throw new Error(`ForNode items (${TSOverviewGenerator.preview.expr(this.itemsExpr)}) return a non-array result!`);
         
@@ -46,10 +60,10 @@ export class ForNode implements ITemplateNode {
             if (this.joiner !== null && result !== "")
                 result += this.joiner;
 
-            model.props[this.variableName] = item;
-            result += this.body.format(model);
+            context.model.props[this.variableName] = item;
+            result += this.body.format(context);
         }
-        delete model.props[this.variableName];
+        delete context.model.props[this.variableName];
         return result;
     }
 }
