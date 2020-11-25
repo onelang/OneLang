014d81fe1e03eeb5cf55c96b4f9e7c32692d8e7d
KT <koczkatamas@gmail.com>
2020-11-25 03:01:49 +0100

    [Gen/Java] JsToJava migration phase 1 is finished

--- src/Generator/JavaPlugins/JsToJava.ts
+++ src/Generator/JavaPlugins/JsToJava.ts
@@ -1,10 +1,9 @@
 import { IGeneratorPlugin } from "../IGeneratorPlugin";
-import { InstanceMethodCallExpression, Expression, StaticMethodCallExpression, RegexLiteral, ElementAccessExpression, ArrayLiteral } from "../../One/Ast/Expressions";
+import { InstanceMethodCallExpression, Expression, StaticMethodCallExpression, RegexLiteral } from "../../One/Ast/Expressions";
 import { Statement } from "../../One/Ast/Statements";
-import { ClassType, InterfaceType, LambdaType, TypeHelper } from "../../One/Ast/AstTypes";
-import { Class, Lambda, Method } from "../../One/Ast/Types";
-import { InstanceFieldReference, InstancePropertyReference, VariableDeclarationReference, VariableReference } from "../../One/Ast/References";
-import { IExpression, IType } from "../../One/Ast/Interfaces";
+import { ClassType } from "../../One/Ast/AstTypes";
+import { Class, Method } from "../../One/Ast/Types";
+import { IExpression } from "../../One/Ast/Interfaces";
 import { JavaGenerator } from "../JavaGenerator";
 
 export class JsToJava implements IGeneratorPlugin {
@@ -12,30 +11,10 @@ export class JsToJava implements IGeneratorPlugin {
 
     constructor(public main: JavaGenerator) { }
 
-    isArray(arrayExpr: Expression) {
-        // TODO: InstanceMethodCallExpression is a hack, we should introduce real stream handling
-        return arrayExpr instanceof VariableReference && !arrayExpr.getVariable().mutability.mutated ||
-            arrayExpr instanceof StaticMethodCallExpression || arrayExpr instanceof InstanceMethodCallExpression;
-    }
-
-    arrayStream(arrayExpr: Expression) {
-        const isArray = this.isArray(arrayExpr);
-        const objR = this.main.expr(arrayExpr);
-        if (isArray)
-            this.main.imports.add("java.util.Arrays");
-        return isArray ? `Arrays.stream(${objR})` : `${objR}.stream()`;
-    }
-
-    toArray(arrayType: IType, typeArgIdx: number = 0) {
-        const type = (<ClassType>arrayType).typeArguments[typeArgIdx];
-        return `toArray(${this.main.type(type)}[]::new)`;
-    }
-
-    convertMethod(cls: Class, obj: Expression, method: Method, args: Expression[], returnType: IType): string {
+    convertMethod(cls: Class, obj: Expression, method: Method, args: Expression[]): string {
         const objR = obj === null ? null : this.main.expr(obj);
         const argsR = args.map(x => this.main.expr(x));
-        if (cls.name === "TsArray") {
-        } else if (cls.name === "TsString") {
+        if (cls.name === "TsString") {
             if (method.name === "replace") {
                 if (args[0] instanceof RegexLiteral) {
                     this.main.imports.add("java.util.regex.Pattern");
@@ -43,45 +22,6 @@ export class JsToJava implements IGeneratorPlugin {
                 }
 
                 return `${argsR[0]}.replace(${objR}, ${argsR[1]})`;
-            }
-        } else if (cls.name === "TsMap" || cls.name === "Map") {
-            if (method.name === "set") {
-                return `${objR}.put(${argsR[0]}, ${argsR[1]})`;
-            } else if (method.name === "get") {
-                return `${objR}.get(${argsR[0]})`;
-            } else if (method.name === "hasKey" || method.name === "has") {
-                return `${objR}.containsKey(${argsR[0]})`;
-            } else if (method.name === "delete") {
-                return `${objR}.remove(${argsR[0]})`;
-            } else if (method.name === "values") {
-                return `${objR}.values().${this.toArray(obj.getType(), 1)}`;
-            }
-        } else if (cls.name === "Object") {
-            if (method.name === "keys") {
-                return `${argsR[0]}.keySet().toArray(String[]::new)`;
-            } else if (method.name === "values") {
-                return `${argsR[0]}.values().${this.toArray(args[0].getType())}`;
-            }
-        } else if (cls.name === "Set") {
-            if (method.name === "values") {
-                return `${objR}.${this.toArray(obj.getType())}`;
-            } else if (method.name === "has") {
-                return `${objR}.contains(${argsR[0]})`;
-            } else if (method.name === "add") {
-                return `${objR}.add(${argsR[0]})`;
-            }
-        } else if (cls.name === "ArrayHelper") {
-        } else if (cls.name === "Array") {
-            if (method.name === "from") {
-                return `${argsR[0]}`;
-            }
-        } else if (cls.name === "Promise") {
-            if (method.name === "resolve") {
-                return `${argsR[0]}`;
-            }
-        } else if (cls.name === "RegExpExecArray") {
-            if (method.name === "get") {
-                return `${objR}[${argsR[0]}]`;
             }
         } else if (["console", "RegExp"].includes(cls.name)) {
             this.main.imports.add(`io.onelang.std.core.${cls.name}`);
@@ -98,19 +38,9 @@ export class JsToJava implements IGeneratorPlugin {
 
     expr(expr: IExpression): string {
         if (expr instanceof InstanceMethodCallExpression && expr.object.actualType instanceof ClassType) {
-            return this.convertMethod(expr.object.actualType.decl, expr.object, expr.method, expr.args, expr.actualType);
-        } else if (expr instanceof InstancePropertyReference && expr.object.actualType instanceof ClassType) {
-            if (expr.property.parentClass.name === "TsString" && expr.property.name === "length")
-                return `${this.main.expr(expr.object)}.length()`;
-            if (expr.property.parentClass.name === "TsArray" && expr.property.name === "length")
-                return `${this.main.expr(expr.object)}.${this.isArray(expr.object) ? "length" : "size()"}`;
-        } else if (expr instanceof InstanceFieldReference && expr.object.actualType instanceof ClassType) {
-            if (expr.field.parentInterface.name === "RegExpExecArray" && expr.field.name === "length")
-                return `${this.main.expr(expr.object)}.length`;
-            if (expr.field.parentInterface.name === "Map" && expr.field.name === "size")
-                return `${this.main.expr(expr.object)}.size()`;
+            return this.convertMethod(expr.object.actualType.decl, expr.object, expr.method, expr.args);
         } else if (expr instanceof StaticMethodCallExpression && expr.method.parentInterface instanceof Class) {
-            return this.convertMethod(expr.method.parentInterface, null, expr.method, expr.args, expr.actualType);
+            return this.convertMethod(expr.method.parentInterface, null, expr.method, expr.args);
         }
         return null;
     }
@@ -118,5 +48,4 @@ export class JsToJava implements IGeneratorPlugin {
     stmt(stmt: Statement): string {
         return null;
     }
-
 }
