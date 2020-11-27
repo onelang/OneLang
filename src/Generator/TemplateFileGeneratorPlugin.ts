f946579c123f4916598ba5230436074525cd3a61
KT <koczkatamas@gmail.com>
2020-11-27 20:52:01 +0100

    [Gen/TemplateFile] Support method overloads with if expressions

--- src/Generator/TemplateFileGeneratorPlugin.ts
+++ src/Generator/TemplateFileGeneratorPlugin.ts
@@ -5,15 +5,17 @@ import { IExpression, IType } from "../One/Ast/Interfaces";
 import { Statement } from "../One/Ast/Statements";
 import { IGeneratorPlugin } from "./IGeneratorPlugin";
 import { Reader } from "../Parsers/Common/Reader";
-import { ICallableValue, IVMValue, ObjectValue } from "../VM/Values";
+import { BooleanValue, ICallableValue, IVMValue, ObjectValue, StringValue } from "../VM/Values";
 import { IInstanceMemberReference, InstanceFieldReference, InstancePropertyReference, StaticFieldReference, StaticPropertyReference, VariableReference } from "../One/Ast/References";
 import { IClassMember } from "../One/Ast/Types";
 import { TemplateParser } from "../Template/TemplateParser";
 import { IGenerator } from "./IGenerator";
-import { IVMHooks, VMContext } from "../VM/ExprVM";
+import { ExprVM, IVMHooks, VMContext } from "../VM/ExprVM";
+import { TypeScriptParser2 } from "../Parsers/TypeScriptParser";
+import { ClassType, TypeHelper } from "../One/Ast/AstTypes";
 
 export class CodeTemplate {
-    constructor(public template: string, public includes: string[]) { }
+    constructor(public template: string, public includes: string[], public ifExpr: Expression) { }
 }
 
 export class CallTemplate {
@@ -49,7 +51,7 @@ export class LambdaValue implements ICallableValue {
 }
 
 export class TemplateFileGeneratorPlugin implements IGeneratorPlugin, IVMHooks {
-    methods: { [name: string]: CallTemplate } = {};
+    methods: { [name: string]: CallTemplate[] } = {};
     fields: { [name: string]: FieldAccessTemplate } = {};
     modelGlobals: { [name: string]: IVMValue } = {};
 
@@ -59,9 +61,11 @@ export class TemplateFileGeneratorPlugin implements IGeneratorPlugin, IVMHooks {
 
         for (const exprStr of Object.keys(exprDict)) {
             const val = exprDict[exprStr];
+            const ifStr = val.str("if");
+            const ifExpr = ifStr === null ? null : new TypeScriptParser2(ifStr, null).parseExpression();
             const tmpl = val.type() === ValueType.String ? 
-                new CodeTemplate(val.asStr(), []) : 
-                new CodeTemplate(val.str("template"), val.strArr("includes"));
+                new CodeTemplate(val.asStr(), [], null) : 
+                new CodeTemplate(val.str("template"), val.strArr("includes"), ifExpr);
 
             this.addExprTemplate(exprStr, tmpl);
         }
@@ -83,6 +87,11 @@ export class TemplateFileGeneratorPlugin implements IGeneratorPlugin, IVMHooks {
         return null;
     }
 
+    addMethod(name: string, callTmpl: CallTemplate): void {
+        if (!(name in this.methods)) this.methods[name] = [];
+        this.methods[name].push(callTmpl);
+    }
+
     addExprTemplate(exprStr: string, tmpl: CodeTemplate): void {
         const expr = new ExpressionParser(new Reader(exprStr)).parse();
         if (expr instanceof UnresolvedCallExpression
@@ -90,11 +99,11 @@ export class TemplateFileGeneratorPlugin implements IGeneratorPlugin, IVMHooks {
               && expr.func.object instanceof Identifier) {
             const callTmpl = new CallTemplate(expr.func.object.text, 
                 expr.func.propertyName, expr.args.map(x => (<Identifier>x).text), tmpl);
-            this.methods[`${callTmpl.className}.${callTmpl.methodName}@${callTmpl.args.length}`] = callTmpl;
+            this.addMethod(`${callTmpl.className}.${callTmpl.methodName}@${callTmpl.args.length}`, callTmpl);
         } else if (expr instanceof UnresolvedCallExpression
               && expr.func instanceof Identifier) {
             const callTmpl = new CallTemplate(null, expr.func.text, expr.args.map(x => (<Identifier>x).text), tmpl);
-            this.methods[`${callTmpl.methodName}@${callTmpl.args.length}`] = callTmpl;
+            this.addMethod(`${callTmpl.methodName}@${callTmpl.args.length}`, callTmpl);
         } else if (expr instanceof PropertyAccessExpression && expr.object instanceof Identifier) {
             const fieldTmpl = new FieldAccessTemplate(expr.object.text, expr.propertyName, tmpl);
             this.fields[`${fieldTmpl.className}.${fieldTmpl.fieldName}`] = fieldTmpl;
@@ -103,23 +112,40 @@ export class TemplateFileGeneratorPlugin implements IGeneratorPlugin, IVMHooks {
     }
 
     expr(expr: IExpression): string {
-        var codeTmpl: CodeTemplate = null;
-        var model: { [name: string]: IVMValue } = {};
+        const isCallExpr = expr instanceof StaticMethodCallExpression || expr instanceof InstanceMethodCallExpression || expr instanceof GlobalFunctionCallExpression;
+        const isFieldRef = expr instanceof StaticFieldReference || expr instanceof StaticPropertyReference ||
+            expr instanceof InstanceFieldReference || expr instanceof InstancePropertyReference;
 
-        if (expr instanceof StaticMethodCallExpression || expr instanceof InstanceMethodCallExpression || expr instanceof GlobalFunctionCallExpression) {
+        if (!isCallExpr && !isFieldRef) return null; // quick return
+
+        let codeTmpl: CodeTemplate = null;
+        const model: { [name: string]: IVMValue } = {};
+        const context = new VMContext(new ObjectValue(model), this);
+
+        model["type"] = new TypeValue(expr.getType());
+        for (const name of Object.keys(this.modelGlobals))
+            model[name] = this.modelGlobals[name];
+
+        if (isCallExpr) {
             const call = <ICallExpression>expr;
             const parentIntf = call.getParentInterface();
             const methodName = `${parentIntf === null ? "" : `${parentIntf.name}.`}${call.getName()}@${call.args.length}`;
-            const callTmpl = this.methods[methodName]||null;
-            if (callTmpl === null) return null;
+            const callTmpls = this.methods[methodName]||null;
+            if (callTmpls === null) return null;
 
-            if (expr instanceof InstanceMethodCallExpression)
-                model["this"] = new ExpressionValue(expr.object);
-            for (let i = 0; i < callTmpl.args.length; i++)
-                model[callTmpl.args[i]] = new ExpressionValue(call.args[i]);
-            codeTmpl = callTmpl.template;
-        } else if (expr instanceof StaticFieldReference || expr instanceof StaticPropertyReference ||
-                expr instanceof InstanceFieldReference || expr instanceof InstancePropertyReference) {
+            for (const callTmpl of callTmpls) {
+                if (expr instanceof InstanceMethodCallExpression)
+                    model["this"] = new ExpressionValue(expr.object);
+                for (let i = 0; i < callTmpl.args.length; i++)
+                    model[callTmpl.args[i]] = new ExpressionValue(call.args[i]);
+
+                if (callTmpl.template.ifExpr === null ||
+                    (<BooleanValue>new ExprVM(context).evaluate(callTmpl.template.ifExpr)).value) {
+                        codeTmpl = callTmpl.template;
+                        break;
+                    }
+            }
+        } else if (isFieldRef) {
             const cm = <IClassMember><any>(<VariableReference>expr).getVariable();
             const field = this.fields[`${cm.getParentInterface().name}.${cm.name}`]||null;
             if (field === null) return null;
@@ -130,15 +156,13 @@ export class TemplateFileGeneratorPlugin implements IGeneratorPlugin, IVMHooks {
         } else
             return null;
 
-        model["type"] = new TypeValue(expr.getType());
-        for (const name of Object.keys(this.modelGlobals))
-            model[name] = this.modelGlobals[name];
+        if (codeTmpl === null) return null;
 
         for (const inc of codeTmpl.includes || [])
             this.generator.addInclude(inc);
 
         const tmpl = new TemplateParser(codeTmpl.template).parse();
-        const result = tmpl.format(new VMContext(new ObjectValue(model), this));
+        const result = tmpl.format(context);
         return result;
     }
 
