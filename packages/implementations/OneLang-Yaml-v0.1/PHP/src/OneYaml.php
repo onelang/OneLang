fc07338295e9e14086167d164e285030fe446f82
KT <koczkatamas@gmail.com>
2020-12-04 17:50:54 +0100

    [pkg/Yaml] Add PHP implementation

--- packages/implementations/OneLang-Yaml-v0.1/PHP/src/OneYaml.php
+++ packages/implementations/OneLang-Yaml-v0.1/PHP/src/OneYaml.php
@@ -2,15 +2,35 @@
 
 namespace OneLang\Yaml;
 
+class ValueType {
+    const NULL = 0;
+    const BOOLEAN = 1;
+    const NUMBER = 2;
+    const STRING = 3;
+    const ARRAY = 4;
+    const OBJECT = 5;
+}
+
 class YamlValue {
     function __construct($value) {
         //var_dump($value);
         $this->value = $value;
     }
 
-    function dbl($key) { return @$this->value[$key] ?? 0; }
-    function str($key) { return @$this->value[$key] ?? null; }
-    function strArr($key) { return $this->value[$key] ?? []; }
+    function type() {
+        if (is_null($this->value)) return ValueType::NULL;
+        if (is_bool($this->value)) return ValueType::BOOLEAN;
+        if (is_int($this->value) || is_float($this->value)) return ValueType::NUMBER;
+        if (is_string($this->value)) return ValueType::STRING;
+        if (is_array($this->value)) return ValueType::ARRAY;
+        return ValueType::OBJECT;
+    }
+
+    function asStr() { return $this->value; }
+
+    function dbl($key) { return array_key_exists($key, $this->value) ? $this->value[$key] : 0; }
+    function str($key) { return array_key_exists($key, $this->value) ? $this->value[$key] : null; }
+    function strArr($key) { return array_key_exists($key, $this->value) ? $this->value[$key] : []; }
     function arr($key) { return array_map(function($x) { return new YamlValue($x); }, $this->value[$key] ?? []); }
     function dict($key) { return $this->arr($key); }
     function obj($key) { return new YamlValue($this->value[$key]); }
