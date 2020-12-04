828586028504d7c5c238e21864348b5b94382c14
KT <koczkatamas@gmail.com>
2020-12-04 17:48:39 +0100

    [pkg/File] PHP copy now can create dirs, fix listFiles / handling

--- packages/implementations/OneLang-File-v0.1/PHP/src/OneFile.php
+++ packages/implementations/OneLang-File-v0.1/PHP/src/OneFile.php
@@ -6,6 +6,8 @@ class OneFile {
     static function listFiles($dir, $recursive) {
         if (!$recursive)
             throw "Not supported";
+        if (substr($dir, -1) !== "/")
+            $dir .= "/";
 
         $rii = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
 
@@ -22,11 +24,19 @@ class OneFile {
         return file_get_contents($fn);
     }
 
-    static function writeText($fn, $contents) {
-        //print("writing file: $fn\n");
+    static function providePath($fn) { 
         $dir = dirname($fn);
         if (!file_exists($dir))
             mkdir($dir, 0777, true);
-        file_put_contents($fn, $contents);
+        return $fn;
+    }
+
+    static function writeText($fn, $contents) {
+        //print("writing file: $fn\n");
+        file_put_contents(OneFile::providePath($fn), $contents);
+    }
+
+    static function copy($src, $dst) {
+        copy($src, OneFile::providePath($dst));
     }
 }
