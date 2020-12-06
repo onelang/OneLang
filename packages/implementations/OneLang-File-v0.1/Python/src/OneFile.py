7e9b0191a03ff34fdab2c872080a4becc45f7bdf
KT <koczkatamas@gmail.com>
2020-12-06 19:50:54 +0100

    [pkg/File] Python: fix CRLF handling, listFiles and impl. copy

--- packages/implementations/OneLang-File-v0.1/Python/src/OneFile.py
+++ packages/implementations/OneLang-File-v0.1/Python/src/OneFile.py
@@ -1,15 +1,20 @@
-import glob
+import pathlib
 import os
+import shutil
 
 class OneFile:
     def read_text(fn):
-        with open(fn, 'r') as f: return f.read()
+        with open(fn, newline='') as f: return f.read()
     
     def write_text(fn, content):
         os.makedirs(os.path.dirname(fn), exist_ok=True)
         with open(fn, 'w') as f: f.write(content)
 
     def list_files(dir, recursive):
-        files = sorted(glob.glob(dir + "**/*", recursive=recursive))
-        files = [x[len(dir):] for x in files if os.path.isfile(x)]
+        files = list(sorted([str(x.relative_to(dir)) for x in pathlib.Path(dir).glob("**/*") if x.is_file()]))
         return files
+
+    def copy(src, dst):
+        os.makedirs(os.path.dirname(dst), exist_ok=True)
+        shutil.copy(src, dst)
+
