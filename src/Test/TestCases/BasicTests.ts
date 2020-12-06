db1abb52543165d4a9a5efc88e51679711bd12f4
KT <koczkatamas@gmail.com>
2020-12-06 15:07:34 +0100

    [Test/BasicTests] Add few test cases around the PHP bug

--- src/Test/TestCases/BasicTests.ts
+++ src/Test/TestCases/BasicTests.ts
@@ -16,11 +16,62 @@ export class BasicTests implements ITestCollection {
         console.log("HelloWorld!");
     }
 
+    printsEscapedString() {
+        console.log("dollar: $");
+        console.log("backslash: \\");
+        console.log("newline: \n");
+        console.log("escaped newline: \\n");
+        console.log("dollar after escape: \\$");
+    }
+
+    printsEscapedTemplateString() {
+        console.log(`dollar: $`);
+        console.log(`backslash: \\`);
+        console.log(`newline: \n`);
+        console.log(`escaped newline: \\n`);
+        console.log(`dollar after escape: \\$`);
+    }
+
+    regexReplace() {
+        console.log("a$b$c".replace(/\$/g, "x"));
+        console.log("Test1: xx".replace(/x/g, "$"));
+        console.log("Test2: yy".replace(/y/g, "\\"));
+        console.log("Test3: zz".replace(/z/g, "\\$"));
+    }
+
+    json() {
+        console.log(JSON.stringify(null));
+        console.log(JSON.stringify(true));
+        console.log(JSON.stringify(false));
+        console.log(JSON.stringify("string"));
+        console.log(JSON.stringify(0.123));
+        console.log(JSON.stringify(123));
+        console.log(JSON.stringify(123.456));
+        console.log(JSON.stringify({ a: "b" }));
+        console.log(JSON.stringify("$"));
+        console.log(JSON.stringify("A \\ B"));
+        console.log(JSON.stringify("A \\\\ B"));
+    }
+
+    phpGeneratorBugs() {
+        console.log("Step1: " + "A $ B");
+        console.log("Step2: " + JSON.stringify("A $ B"));
+        console.log("Step3: " + JSON.stringify("A $ B").replace(/\$/g, "\\$"));
+        console.log("Step3 w/o JSON: " + "A $ B".replace(/\$/g, "\\$"));
+        // for (const expr of ["A $ B", "A \\ B", "A \\\\ B", "A \\$ B"]) 
+        //     console.log(JSON.stringify(expr).replace(/\$/g, "\\$"));
+    }
+
     getTestCases(): TestCase[] {
         return [
             new SyncTestCase("PrintsOneLineString", _ => this.printsOneLineString()),
             new SyncTestCase("PrintsMultiLineString", _ => this.printsMultiLineString()),
-            new SyncTestCase("PrintsTwoStrings", _ => this.printsTwoStrings())
+            new SyncTestCase("PrintsTwoStrings", _ => this.printsTwoStrings()),
+            new SyncTestCase("PrintsEscapedString", _ => this.printsEscapedString()),
+            new SyncTestCase("PrintsEscapedTemplateString", _ => this.printsEscapedTemplateString()),
+            new SyncTestCase("RegexReplace", _ => this.regexReplace()),
+            new SyncTestCase("Json", _ => this.json()),
+            new SyncTestCase("PhpGeneratorBugs", _ => this.phpGeneratorBugs())
         ];
     }
 }
